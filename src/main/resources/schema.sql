83bca128f7c24dc408acfa890482ad519cdf3272
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-21 10:00:27 +0900

    add: deadline and created_at and updated_at on milestones table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,8 @@
 create table milestones (
 id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 title VARCHAR(256) NOT NULl,
-description VARCHAR(256) NOT NULl
+description VARCHAR(256) NOT NULl,
+deadline timestamp NOT NULL default current_timestamp,
+created_at timestamp  default current_timestamp,
+updated_at timestamp default current_timestamp on update current_timestamp
 );
