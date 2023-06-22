9a04f4b4db3cdb052baa91bc922e85582b03cee3
hikaru-WA1st <135082388+hikaru-WA1st@users.noreply.github.com>
2023-06-22 11:09:25 +0900

    shemaのsyntaxerror直し

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -41,7 +41,7 @@ id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL,
 created_at timestamp default current_timestamp,
 updated_at timestamp default current_timestamp on update current_timestamp
-)
+);
 
 create table taskLabels (
 id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -52,4 +52,4 @@ updated_at timestamp default current_timestamp on update current_timestamp,
 
 FOREIGN KEY (task_id) REFERENCES tasks(id),
 FOREIGN KEY (label_id) REFERENCES labels(id)
-)
+);
