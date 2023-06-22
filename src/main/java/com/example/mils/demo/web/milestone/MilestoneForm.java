f8c3907fc7d8d46c904386ec8243592742b8abb1
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-22 11:39:23 +0900

    add: deadlineフィールド, validation

--- src/main/java/com/example/mils/demo/web/milestone/MilestoneForm.java
+++ src/main/java/com/example/mils/demo/web/milestone/MilestoneForm.java
@@ -1,6 +1,9 @@
 package com.example.mils.demo.web.milestone;
 
+import java.time.LocalDate;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
@@ -16,4 +19,8 @@ public class MilestoneForm {
     @NotBlank
     @Size(max = 256)
     private String description;
+
+    @NotNull
+    @Future // 未来の日付であることをバリデーション
+    private LocalDate deadline;
 }
