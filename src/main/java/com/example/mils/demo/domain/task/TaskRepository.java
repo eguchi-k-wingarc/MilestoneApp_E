0062c21dd1493e707ed13900339a9f0329169a1c
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-22 11:41:03 +0900

    create: src/main/java/com/example/mils/demo/domain/task/TaskRepository.java

---
+++ src/main/java/com/example/mils/demo/domain/task/TaskRepository.java
@@ -0,0 +1,21 @@
+package com.example.mils.demo.domain.task;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface TaskRepository {
+    @Select("select * from tasks")
+    List<TaskEntity> findAll();
+
+    @Select("select * from tasks where id = #{id}")
+    TaskEntity findById(@Param("id") long id);
+
+    @Select("select * from tasks where id = #{milestoneId}")
+    List<TaskEntity> findByMilestoneId(@Param("milestoneId") long milestoneId);
+
+    @Insert("INSERT INTO tasks (name, description) VALUES (#{name}, #{description})")
+    void create(@Param("name") String name, @Param("description") String description);
+}
