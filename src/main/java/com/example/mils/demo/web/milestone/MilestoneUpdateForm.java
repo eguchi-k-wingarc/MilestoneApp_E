02dfa1e5c3cb57868d8f06498b39d39016515d97
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-22 14:46:44 +0900

    create: src/main/java/com/example/mils/demo/web/milestone/MilestoneUpda

---
+++ src/main/java/com/example/mils/demo/web/milestone/MilestoneUpdateForm.java
@@ -0,0 +1,33 @@
+package com.example.mils.demo.web.milestone;
+
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class MilestoneUpdateForm {
+    @NotBlank
+    private Long id; // NOTE: インスタンス生成時のみNullを許容するためlong型ではなく、Long型にしている
+
+    @NotBlank
+    @Size(max = 256)
+    private String name;
+
+    @NotBlank
+    @Size(max = 256)
+    private String description;
+
+    @NotNull
+    @Future // 未来の日付であることをバリデーション
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime deadline;
+}
