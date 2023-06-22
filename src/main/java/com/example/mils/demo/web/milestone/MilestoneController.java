f90ba73d23acbc3cc0f89fb9d9f2f3fccfa145ad
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-22 10:23:48 +0900

    add: コメント in MilestoneController

--- src/main/java/com/example/mils/demo/web/milestone/MilestoneController.java
+++ src/main/java/com/example/mils/demo/web/milestone/MilestoneController.java
@@ -16,7 +16,6 @@ import com.example.mils.demo.domain.milestone.MilestoneService;
 
 import lombok.AllArgsConstructor;
 
-
 @Controller
 @AllArgsConstructor
 @RequestMapping("/milestones")
@@ -24,6 +23,12 @@ public class MilestoneController {
 
     private final MilestoneService milestoneService;
 
+    /**
+     * 一覧画面を表示します。
+     *
+     * @param model 画面に渡すデータを格納するModelオブジェクト
+     * @return 一覧画面のテンプレート名
+     */
     @GetMapping()
     public String showList(Model model) {
         List<MilestoneEntity> milestoneList = milestoneService.findAll();
@@ -33,6 +38,13 @@ public class MilestoneController {
         return "milestones/list";
     }
 
+    /**
+     * 指定されたIDのマイルストーンの詳細画面を表示します。
+     *
+     * @param id    マイルストーンのID
+     * @param model 画面に渡すデータを格納するModelオブジェクト
+     * @return 詳細画面のテンプレート名
+     */
     @GetMapping("/{id}")
     public String showDetail(@PathVariable("id") long id, Model model) {
         MilestoneEntity milestone = milestoneService.findById(id);
@@ -41,11 +53,24 @@ public class MilestoneController {
         return "milestones/detail";
     }
 
+    /**
+     * マイルストーンの作成フォームを表示します。
+     *
+     * @param creationForm マイルストーン作成フォームオブジェクト
+     * @return 作成フォームのテンプレート名
+     */
     @GetMapping("/create")
     public String showCreationForm(@ModelAttribute MilestoneForm creationForm) {
         return "milestones/creationForm";
     }
 
+    /**
+     * マイルストーンを作成します。
+     *
+     * @param creationForm  マイルストーン作成フォームオブジェクト
+     * @param bindingResult バリデーション結果を保持するBindingResultオブジェクト
+     * @return 一覧画面にリダイレクトするURL
+     */
     @PostMapping
     public String create(@Validated MilestoneForm creationForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
