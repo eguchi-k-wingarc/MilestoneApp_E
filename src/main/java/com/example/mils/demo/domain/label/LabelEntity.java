633962894ec075e1893b0caac8bd2ed6df90eab2
hikaru-WA1st <135082388+hikaru-WA1st@users.noreply.github.com>
2023-06-22 11:49:44 +0900

    add: LabelEntity, LabelRepository, LabelService

---
+++ src/main/java/com/example/mils/demo/domain/label/LabelEntity.java
@@ -0,0 +1,11 @@
+package com.example.mils.demo.domain.label;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class LabelEntity {
+    private long id;
+    private String name;
+}
