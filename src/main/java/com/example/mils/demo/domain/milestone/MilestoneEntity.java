83fe7d37fd3caeb06fda94e0e2d6acebff6a92c1
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-22 11:36:25 +0900

    modify: MilestoneEntityの修正

--- src/main/java/com/example/mils/demo/domain/milestone/MilestoneEntity.java
+++ src/main/java/com/example/mils/demo/domain/milestone/MilestoneEntity.java
@@ -7,6 +7,12 @@ import lombok.Data;
 @Data
 public class MilestoneEntity {
     private long id;
+    private long userId;
     private String name;
     private String description;
+    private boolean isComplete;
+    private int progress;
+    private String deadline;
+    private String createdAt;
+    private String updatedAt;
 }
