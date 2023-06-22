e6c2dc7d0686a9e4b4f7a1a4fa329d4bcadcbf45
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-22 17:41:22 +0900

    add: タスクの詳細機能追加

--- src/main/java/com/example/mils/demo/domain/task/TaskRepository.java
+++ src/main/java/com/example/mils/demo/domain/task/TaskRepository.java
@@ -1,11 +1,14 @@
 package com.example.mils.demo.domain.task;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface TaskRepository {
@@ -18,6 +21,17 @@ public interface TaskRepository {
     @Select("select * from tasks where milestone_id = #{milestoneId}")
     List<TaskEntity> findByMilestoneId(@Param("milestoneId") Long milestoneId); // TODO: nullを許可しないlong型に変更する
 
-    @Insert("INSERT INTO tasks (name, description) VALUES (#{name}, #{description})")
-    void create(@Param("name") String name, @Param("description") String description);
+    @Insert("INSERT INTO tasks (milestone_id, name, description, deadline) VALUES (#{milestoneId}, #{name}, #{description}, #{deadline})")
+    void create(@Param("milestoneId") long milestoneId, @Param("name") String name,
+            @Param("description") String description, @Param("deadline") LocalDateTime deadline);
+
+    @Update("UPDATE tasks SET name = #{name}, description = #{description}, deadline = #{deadline} WHERE id = #{id}")
+    void update(
+            @Param("id") long id,
+            @Param("name") String name,
+            @Param("description") String description,
+            @Param("deadline") LocalDateTime deadline);
+
+    @Delete("DELETE FROM tasks WHERE id = #{id}")
+    void delete(@Param("id") long id);
 }
