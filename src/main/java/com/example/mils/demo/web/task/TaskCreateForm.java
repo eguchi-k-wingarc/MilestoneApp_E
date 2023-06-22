91dc6b52c72e08e3a1d276d25c6a8ffe70fce787
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-22 17:39:52 +0900

    create: src/main/java/com/example/mils/demo/web/task/TaskCreateForm.java

---
+++ src/main/java/com/example/mils/demo/web/task/TaskCreateForm.java
@@ -0,0 +1,28 @@
+package com.example.mils.demo.web.task;
+
+import java.time.LocalDateTime;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class TaskCreateForm {
+    @NotBlank
+    @Size(max = 256)
+    private String name;
+
+    @NotBlank
+    @Size(max = 256)
+    private String description;
+
+    @NotNull
+    @Future // 未来の日付であることをバリデーション
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDateTime deadline;
+}
