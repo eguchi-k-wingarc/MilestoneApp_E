4f4b92987e7ae037003a72409ce304610d85120d
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-22 11:44:55 +0900

    create: src/main/java/com/example/mils/demo/domain/task/TaskService.java

---
+++ src/main/java/com/example/mils/demo/domain/task/TaskService.java
@@ -0,0 +1,31 @@
+package com.example.mils.demo.domain.task;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public List<TaskEntity> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public TaskEntity findById(long id) {
+        return taskRepository.findById(id);
+    }
+
+    public List<TaskEntity> findByMilestoneId(long milestoneId) {
+        return taskRepository.findByMilestoneId(milestoneId);
+    }
+
+    @Transactional
+    public void create(String name, String description) {
+        taskRepository.create(name, description);
+    }
+}
