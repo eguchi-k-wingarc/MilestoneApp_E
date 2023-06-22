21939a10d9b5ab756a06cc01c39ef4fff39f7ad7
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-22 11:49:01 +0900

    modify: bean追加

--- src/main/java/com/example/mils/demo/domain/task/TaskRepository.java
+++ src/main/java/com/example/mils/demo/domain/task/TaskRepository.java
@@ -3,9 +3,11 @@ package com.example.mils.demo.domain.task;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+@Mapper
 public interface TaskRepository {
     @Select("select * from tasks")
     List<TaskEntity> findAll();
