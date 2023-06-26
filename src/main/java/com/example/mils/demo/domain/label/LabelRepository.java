0dda5d9166186c3917af9a09e15ef8dc91ccbe64
hikaru-WA1st <135082388+hikaru-WA1st@users.noreply.github.com>
2023-06-26 13:54:08 +0900

    pulled

--- src/main/java/com/example/mils/demo/domain/label/LabelRepository.java
+++ src/main/java/com/example/mils/demo/domain/label/LabelRepository.java
@@ -1,6 +1,5 @@
 package com.example.mils.demo.domain.label;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
@@ -15,6 +14,7 @@ import org.apache.ibatis.annotations.Update;
 public interface LabelRepository {  //ラベル操作用インターフェース
 
     //全てのラベルを取得する
+    //return List<LabelEntity>
     @Select("select * from labels")
     List<LabelEntity> findAll();
 
@@ -32,5 +32,5 @@ public interface LabelRepository {  //ラベル操作用インターフェース
     );
     
     @Delete("DELETE FROM labels WHERE id = #{id}")
-    void delete(@Param("id") Long id);
+    void delete(@Param("id") long id);
 }
--- src/main/java/com/example/mils/demo/web/label/LabelController.java
+++ src/main/java/com/example/mils/demo/web/label/LabelController.java
@@ -8,11 +8,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.mils.demo.domain.label.LabelEntity;
 import com.example.mils.demo.domain.label.LabelService;
+import com.example.mils.demo.web.milestone.MilestoneUpdateForm;
 
 import lombok.AllArgsConstructor;
 
@@ -61,6 +63,57 @@ public class LabelController {
             return showCreationForm(creationForm);
         }
         labelService.create(creationForm.getName(), creationForm.getColor());
+        return "redirect:/labels";
+    }
+
+    /**
+     * ラベルの編集フォームを表示します。
+     *
+     * @param labelId ラベルのID
+     * @param model       画面に渡すデータを格納するModelオブジェクト
+     * @return 編集フォームのテンプレート名
+     */
+    @GetMapping("/{labelId}/update")
+    public String showEditForm(@PathVariable("labelId") Long labelId, LabelUpdateForm form, Model model) {
+        LabelEntity label = labelService.findById(labelId);
+        if (label != null) {
+            form.setName(label.getName());
+            form.setColor(label.getColor());
+        } else {
+            return "redirect:/labels";
+        }
+        model.addAttribute("labelUpdateForm", form);
+        return "labels/updateForm";
+    }
+
+    /**
+     * ラベルを更新します。
+     *
+     * @param labelId   マイルストーンのID
+     * @param form          マイルストーン編集フォームオブジェクト
+     * @param bindingResult バリデーション結果を保持するBindingResultオブジェクト
+     * @return 一覧画面にリダイレクトするURL
+     */
+    @PostMapping("/{labelId}/update")
+    public String update(@PathVariable("labelId") Long labelId, @Validated LabelUpdateForm form,
+            BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return showEditForm(labelId, form, model);
+        }
+
+        labelService.update(labelId, form.getName(), form.getColor());
+        return "redirect:/labels";
+    }
+
+    /**
+     * ラベルを削除します。
+     *
+     * @param rabelId ラベルのID
+     * @return 一覧画面にリダイレクトするURL
+     */
+    @PostMapping("/{labelId}/delete")
+    public String delete(@PathVariable("labelId") Long labelId) {
+        labelService.delete(labelId);
         return "redirect:/labels";
     }
 
