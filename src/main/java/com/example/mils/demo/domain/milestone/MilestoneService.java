b13d1a6ad31ca4b4c9ac7060ef6ae4abb6c777ef
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-27 11:56:57 +0900

    add: デスマーチの反映

--- src/main/java/com/example/mils/demo/domain/milestone/MilestoneService.java
+++ src/main/java/com/example/mils/demo/domain/milestone/MilestoneService.java
@@ -1,40 +1,33 @@
 package com.example.mils.demo.domain.milestone;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import com.example.mils.demo.domain.DeathLevelOperator;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class MilestoneService {
     private final MilestoneRepository milestoneRepository;
+    private final DeathLevelOperator deathLevelOperator;
 
-    public List<MilestoneEntity> findAll() {
-        List<MilestoneEntity> list = milestoneRepository.findAll();
-        List<MilestoneEntity> processedList = list.stream().map(milestone ->{
-            LocalDateTime today = LocalDateTime.now();
-            LocalDateTime deadline = milestone.getDeadline();
-            Duration duration = Duration.between(today, deadline).dividedBy((long)10);
-            milestone.setDeadline(today.plus(duration));
-            return milestone;
-        }).collect(Collectors.toList());
-        return processedList;
-    }
-
-    public MilestoneEntity findById(long id) {
-        MilestoneEntity milestone = milestoneRepository.findById(id);
-        LocalDateTime today = LocalDateTime.now();
-        LocalDateTime deadline = milestone.getDeadline();
-        Duration duration = Duration.between(today, deadline).dividedBy((long)10);
-        milestone.setDeadline(today.plus(duration));
+public List<MilestoneEntity> findAll() {
+    List<MilestoneEntity> list = milestoneRepository.findAll();
+    List<MilestoneEntity> processedList = list.stream().map(milestone ->{
+        milestone.setDeadline(deathLevelOperator.adjustDeadline(milestone.getDeadline()));
         return milestone;
-    }
+    }).collect(Collectors.toList());
+    return processedList;
+}
+
+public MilestoneEntity findById(long id) {
+    MilestoneEntity milestone = milestoneRepository.findById(id);
+    milestone.setDeadline(deathLevelOperator.adjustDeadline(milestone.getDeadline()));
+    return milestone;
+}
 
     @Transactional
     public void create(String name, String description, LocalDateTime deadline) {
