2853f40e1dabb4e4890386d98350cbae33e1af59
kanmatsu-wa <matsumoto.ka@wingarc.com>
2023-06-27 16:52:44 +0900

    update: organized user domain

--- src/main/java/com/example/mils/demo/web/IndexController.java
+++ src/main/java/com/example/mils/demo/web/IndexController.java
@@ -4,15 +4,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import com.example.mils.demo.domain.user.UserEntity;
-import com.example.mils.demo.domain.user.UserService;
-import com.example.mils.demo.web.user.UserRegisterForm;
 
 import lombok.AllArgsConstructor;
 
@@ -20,7 +13,6 @@ import lombok.AllArgsConstructor;
 @Controller
 public class IndexController {
 
-    private UserService userService;
 
     @GetMapping
     public String index(Model model,@AuthenticationPrincipal UserDetails loginUser) {
@@ -29,22 +21,11 @@ public class IndexController {
     }
 
     @GetMapping("/login")
-    public String showLoginForm() {
-        return "login";
+    public String redirectLogin(){
+        return "redirect:users/login";
     }
-
     @GetMapping("/register")
-    public String showRegistrationForm(@ModelAttribute UserRegisterForm form, Model model) {
-        return "register";
-    }
-
-    @PostMapping("/register")
-    public String registerUser(@ModelAttribute("user") @Validated UserRegisterForm form, BindingResult bindingResult,
-            Model model) {
-        if (bindingResult.hasErrors()) {
-            return showRegistrationForm(form, model);
-        }
-        userService.create(form.getEmail(), form.getPassword(), UserEntity.DEFAULT_AUTHORITIES);
-        return "redirect:/login";
+    public String redirectRegister(){
+        return "redirect:users/register";
     }
 }
