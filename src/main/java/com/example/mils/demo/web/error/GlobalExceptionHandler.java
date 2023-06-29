2ba487f6dbefe2cacdd7eb0d90d3c76bf02a1d3e
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-29 11:03:31 +0900

    add: エラー画面

--- src/main/java/com/example/mils/demo/web/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/mils/demo/web/error/GlobalExceptionHandler.java
@@ -2,14 +2,31 @@ package com.example.mils.demo.web.error;
 
 import java.nio.file.AccessDeniedException;
 
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public String handleBadRequestException(HttpMessageNotReadableException e) {
+        return "error/400";
+    }
+
     @ExceptionHandler(AccessDeniedException.class)
     public String handleAccessDeniedException(AccessDeniedException e) {
         return "error/403";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String handleNotFoundException(NoHandlerFoundException e) {
+        return "error/404";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e) {
+        return "error/500";
     }
 }
