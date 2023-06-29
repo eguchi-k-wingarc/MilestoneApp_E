e82210e1cf129e3bd993f8f1c25e9bf4c8f48ef6
eguchi-k-wingarc <eguchi.k@wingarc.com>
2023-06-29 09:46:30 +0900

    modify: @RestControllerをつけて、json形式で返却する

--- src/main/java/com/example/mils/demo/web/milestone/api/ApiMilestoneController.java
+++ src/main/java/com/example/mils/demo/web/milestone/api/ApiMilestoneController.java
@@ -1,15 +1,16 @@
 package com.example.mils.demo.web.milestone.api;
 
 import java.util.List;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.mils.demo.domain.milestone.MilestoneProgressDto;
 import com.example.mils.demo.domain.milestone.MilestoneService;
 import lombok.AllArgsConstructor;
 
-@Controller
+@RestController
 @AllArgsConstructor
 @RequestMapping("/api/milestones")
 public class ApiMilestoneController {
